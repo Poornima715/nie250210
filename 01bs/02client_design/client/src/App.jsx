1012efb1ae833bed27dad497af5fc2a7457f821c
Poornima715 <2023cs_poornimadevangamath_c@nie.ac.in>
2025-02-11 12:35:05 +0530

    golang full stack

---
+++ 01bs/02client_design/client/src/App.jsx
@@ -0,0 +1,23 @@
+import PageHeader from "./header/PageHeader"
+import FlightList from "./flights/FlightList"
+import FlightCreate from "./flights/FlightCreate"
+import FlightEdit from "./flights/FlightEdit"
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+function App() {//app is a functional component of UI
+  return (
+    <>
+      <div>
+        <BrowserRouter>
+        <Routes>
+          <Route path="" element={<FlightList/>}/>
+          <Route path="/flights/list" element={<FlightList/>}/>
+          <Route path='/flights/create' element={<FlightCreate/>}/>
+          <Route path='/fligths/edit/:id' element={<FlightEdit/>}/>
+          </Routes></BrowserRouter>
+        </div>
+              
+    </>
+  )
+}
+
+export default App
